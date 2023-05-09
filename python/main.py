d8857d83cdad417a5d4be9d9e3607ab85ce68ba8
Tobi-IT <74515548+Tobi-IT@users.noreply.github.com>
2023-05-09 20:04:27 +0200

    Update main.py
    Now working .CSV download

--- python/main.py
+++ python/main.py
@@ -1,71 +1,27 @@
-import subprocess
-import time
 import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 
-url = "https://www.onlinehashcrack.com/dashboard"
 email = "example@email.com"
 
-# configure Chrome WebDriver, to run in headless mode
-chrome_options = Options()
-chrome_options.add_argument("--headless")
+# Log in
+url_login = "https://www.onlinehashcrack.com/dashboard"
+headers_login = {}
+data_login = {"emailTasks": email, "submit": ""}
+session = requests.Session()
+response_login = session.post(url_login, headers=headers_login, data=data_login)
 
-# Run instance of headless chrome
-driver = webdriver.Chrome(options=chrome_options)
+# Check if the login was successful
+if response_login.status_code == 200:
+    print("Login successful!")
 
-# open url
-driver.get(url)
+    # Download CSV file
+    url_csv = "https://www.onlinehashcrack.com/wpa-exportcsv"
+    response_csv = session.get(url_csv)
 
-# Wait until fully loaded (Highly WIP)
-time.sleep(5)
-
-# Search for the mail form an enter mail adress
-email_input = driver.find_element(By.NAME, "emailTasks")
-email_input.send_keys(email)
-
-# Press enter to sent the requests
-email_input.send_keys(Keys.RETURN)
-
-# wait for successfull login
-time.sleep(5)
-
-# save the current PHPSESSID Cookie
-cookies = driver.get_cookies()
-phpsessid_cookie = None
-
-for cookie in cookies:
-    if cookie["name"] == "PHPSESSID":
-        phpsessid_cookie = cookie["value"]
-        break
-
-if phpsessid_cookie:
-    print("PHPSESSID Cookie:", phpsessid_cookie)
+    if response_csv.status_code == 200:
+        with open("output.csv", "wb") as file:
+            file.write(response_csv.content)
+        print("CSV file successfully downloaded!")
+    else:
+        print("Error downloading the CSV file.")
 else:
-    print("PHPSESSID-Cookie wurde nicht gefunden.")
-
-# Kill browser instance
-driver.quit()
-
-# function to export the csv which holds all hashes
-def download_csv(phpsessid_cookie):
-    csv_url = "https://www.onlinehashcrack.com/wpa-exportcsv"
-    output_file = "output.csv"
-
-    # setup PHPSESSID cookie
-    cookies = {"PHPSESSID": phpsessid_cookie}
-
-    # Request the csv while providing the saved cookie
-    response = requests.get(csv_url, cookies=cookies)
-    with open(output_file, "wb") as f:
-        f.write(response.content)
-
-    print(f"CSV-Datei was created as {output_file} .")
-
-# if cookie is present, call the download_csv function
-if phpsessid_cookie:
-    download_csv(phpsessid_cookie)
-else:
-    print("PHPSESSID-Cookie was not found.")
+    print("Login failed.")
