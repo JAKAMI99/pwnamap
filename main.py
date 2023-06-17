31d55e588bc31b9385ef69c8b40326feb38264c0
JAKAMI99 <schonfurdich@gmail.com>
2023-06-17 05:59:15 +0200

    added: map, geolocation lookup

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-import sys, os, time
-import csv_api as csvapi
+import csv_api
+import map
 
 ##############################################################################################################
 #Provide your mail used for onlinehashcrack.com (Pwnagotchi Plugin)                                         ##
-mail = "mail@example.com"                                                                                   ##
+mail = "CHANGEME"                                                                                           ##
 #Wigle API Token (Sadly by default 10 requests/day)                                                         ##
-api_token = ""                                                                                              ##
+api_token = "CHANGEME"                                                                                      ##
 #Only lookup the geolocation of BSSIDs, that have a "password"-entry (Highly recommend)                     ##
-onlypw = True # Only request geolocation of password is found in CSV (True/False)                           ##
+onlypw = True #[True/False]                                                                                 ##
 ##############################################################################################################
 
 
+
+
+
+
+
+
+#Filenames
 tmpcsv = "ohc-tmp.csv"
 latestcsv = "ohc-latest.csv"
+database = "database.csv"
 
+csv_api.download(mail, tmpcsv) # download the csv from onlinehashcrack.com while providing usermail
+csv_api.compare(tmpcsv, latestcsv) #compare new with old csv to check for changes
+csv_api.wigle(api_token, latestcsv, database, onlypw) # Ask Wigle.net API for geolocations of the BSSIDs collected in out.csv
 
-csvapi.download(mail, tmpcsv) # download the csv from onlinehashcrack.com while providing usermail
-csvapi.compare(tmpcsv, latestcsv) #compare new with old csv to check for changes
-csvapi.wigle(api_token, latestcsv, onlypw) # Ask Wigle.net API for geolocations of the BSSIDs collected in out.csv
+map.create(database)
