c2e23b8240883f8b736430166be1219626e9f44c
JAKAMI99 <schonfurdich@gmail.com>
2023-06-17 05:59:58 +0200

    added map

--- csv_api.py
+++ csv_api.py
@@ -1,9 +1,5 @@
 import requests, filecmp, os, pandas, shutil, html
 
-
-
-# Variables for file operations (comparing/download/rename)
-
 def download(mail, output):
     # Log in
     url_login = "https://www.onlinehashcrack.com/dashboard"
@@ -41,34 +37,36 @@ def compare(tmpcsv, latestcsv):
         print(f"Asuming first pull since no old CSV was found.")
         os.rename(tmpcsv, latestcsv)
 
-def wigle(api_token, latestcsv, onlypw):
+
+def wigle(api_token, latestcsv, database, onlypw):
     try:
-        shutil.copyfile(latestcsv, "database.csv")  # keep original for future comparing for new entries in "compare"
+        shutil.copyfile(latestcsv, database)  # keep original for future comparing for new entries in "compare"
     except FileNotFoundError:
         print(f"File '{latestcsv}' not found")
         return
-
     API_URL = 'https://api.wigle.net/api/v2/network/search'
-
-    data = pandas.read_csv("database.csv", delimiter=',', encoding='unicode_escape')
-    latitudes = []  # Create an empty list to store latitudes
-    longitudes = []  # Create an empty list to store longitudes
+    result_found = False
+    data = pandas.read_csv(database, delimiter=',', encoding='unicode_escape')
+    #creating placeholder
+    latitudes = []
+    longitudes = []
+    firsttimes = []
+    lasttimes = []
+    encryptions  = []
 
     # Iterate over each row in the DataFrame
     for _, row in data.iterrows():
         # Decode HTML entities for the entire content of each field
         decoded_row = [html.unescape(str(field)) for field in row]
-
         # Check if onlypw is False or the entry has a password
-        if not onlypw or decoded_row[4]:
+        if not onlypw or (decoded_row[4] != "nan"):
+            print(f"row contained {decoded_row[4]}")
             # Extract the BSSID value
             bssid = decoded_row[2]
-
-            # Create the request URL for the WIGLE API
+            # Build request
             print(f"Requesting geolocation for {bssid}")
             request_url = f"{API_URL}?netid={bssid}"
-
-            # Send GET request to the WIGLE API
+            # Send request to the API
             headers = {'Authorization': f'Basic {api_token}'}
             response = requests.get(request_url, headers=headers)
             # Process the API response
@@ -76,29 +74,52 @@ def wigle(api_token, latestcsv, onlypw):
                 response_data = response.json()
                 if 'results' in response_data and response_data['results']:
                     result = response_data['results'][0]
+                    print(f"Result for {bssid} was valid: \n{result}")
                     latitude = result['trilat']
                     longitude = result['trilong']
-                    latitudes.append(latitude)  # Add latitude to the list
-                    longitudes.append(longitude)  # Add longitude to the list
-                else:
-                    print(f"Result from Wigle was unvalid")
-                    continue
+                    firsttime = result['firsttime']
+                    lasttime = result['lasttime']
+                    encryption = result['encryption']
+                    latitudes.append(latitude)
+                    longitudes.append(longitude)
+                    firsttimes.append(firsttime)
+                    lasttimes.append(lasttime)
+                    encryptions.append(encryption)
+                    result_found = True
                     
+                else:
+                    print(f"Result from Wigle was unvalid:\n[{response_data}]")
+                    # Set empty values for latitude, longitude, firsttime, lasttime, and encryption
+                    latitudes.append('')
+                    longitudes.append('')
+                    firsttimes.append('')
+                    lasttimes.append('')
+                    encryptions.append('')
             elif response.status_code == 429:
                 print(f"Statuscode was 429 Too many request - API Rate limited... Try again later)")
                 return
             elif response.status_code == 401:
                 print(f"Statuscode was 401 Unauthorized - Validate your API Key)")
             else:
-                print(f"Statuscode was {response.status_code}")
+                print(f"Error - Statuscode was {response.status_code}. Continuing with next entry.")
                 continue
-            print(f"{decoded_row[2]} contained no password. Skipping due to onlypw = True")
         else:
+            print(f"{decoded_row[2]} contained no password. Skipping due to onlypw = True")
+            latitudes.append('')
+            longitudes.append('')
+            firsttimes.append('')
+            lasttimes.append('')
+            encryptions.append('')
             continue
 
-    # Add latitude and longitude columns to the DataFrame
-    data['Latitude'] = latitudes
-    data['Longitude'] = longitudes
-
-    # Save the updated DataFrame back to the CSV file
-    data.to_csv("database.csv", index=False)
+    # Add latitude and longitude columns to the DataFrame if valid entries where found
+    if result_found:
+        data['latitude'] = latitudes
+        data['longitude'] = longitudes
+        data['firsttime'] = firsttimes
+        data['lasttime'] = lasttimes
+        data['encryption'] = encryptions
+        # Save the updated DataFrame back to the CSV file
+        data.to_csv(database, index=False)
+    else:
+        print(f"No valid results were found for entries in {database}")
